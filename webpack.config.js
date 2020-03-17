f8a52d461b0b8c6edd840d4a192f2d356e8f1240
mataotao <mttwind@foxmail.com>
2020-03-17 22:16:15 +0800

    HtmlWebPackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 //common.js规范
 
 const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname,'dist')//打包出来的文件目录地址,需要是绝对路径,__dirname是项目所在的目录
-    }
+    },
+    plugins: [
+        //每次打包后的js要插入到html当中
+        //作用:根据模板的html生成新的html,并把打包后的js 引入到html里面
+        new HtmlWebPackPlugin({
+            template: path.resolve(__dirname,'index.html'),
+            filename: "home.html",//会把打包后的js引入到这个里面来
+        }),
+    ]
 }
+
