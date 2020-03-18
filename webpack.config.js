8cfa4313c59ca0936f05c5a071a5f007380133bb
mataotao <mttwind@foxmail.com>
2020-03-18 13:28:24 +0800

    多入口打包

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,21 @@ const path = require('path')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname,'dist')//打包出来的文件目录地址,需要是绝对路径,__dirname是项目所在的目录
+    //单入口,单出口打包
+    // entry: './src/index.js',
+    // output: {
+    //     filename: "bundle.js",
+    //     path: path.resolve(__dirname,'dist')//打包出来的文件目录地址,需要是绝对路径,__dirname是项目所在的目录
+    // },
+    entry: {
+        index:'./src/index.js',
+        other:'./src/other.js'
     },
+    output:{
+        filename: '[name].js',
+        path: path.resolve(__dirname,'dist')
+    },
+
     devServer: {
         contentBase: path.join(__dirname, 'static'),
         compress: true,//一切服务都启用 gzip 压缩：
@@ -17,12 +27,19 @@ module.exports = {
     },
     plugins: [
         //清空输出目录
-        new CleanWebpackPlugin(),
+        // new CleanWebpackPlugin(),
         //每次打包后的js要插入到html当中
         //作用:根据模板的html生成新的html,并把打包后的js 引入到html里面
         new HtmlWebPackPlugin({
             template: path.resolve(__dirname,'index.html'),
-            filename: "home.html",//会把打包后的js引入到这个里面来
+            filename: "index.html",//会把打包后的js引入到这个里面来
+            chunks: ['index']//表示把名为index的chunk打包进index.html
         }),
+        new HtmlWebPackPlugin({
+            template: path.resolve(__dirname,'other.html'),
+            filename: "other.html",
+            chunks: ['other']
+        }),
+
     ]
 }
