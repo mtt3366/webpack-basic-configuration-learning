e64a7e5ad9483a936d70b4833ce7dc6ad1271ace
mataotao <mttwind@foxmail.com>
2020-03-19 11:32:16 +0800

    css,less

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import sum1 from './sum'
+import './index.css'
+import './a.less'
 console.log(sum1(1,2))
--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@
 const path = require('path')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const htmlPlugin = ['index','other'].map(chunksName=>{
+const htmlPlugin = ['index', 'other'].map(chunksName => {
     return new HtmlWebPackPlugin({
-        template: path.resolve(__dirname,`${chunksName}.html`),
+        template: path.resolve(__dirname, `${chunksName}.html`),
         filename: `${chunksName}.html`,
         chunks: [chunksName]
     })
@@ -18,19 +18,42 @@ module.exports = {
     //     path: path.resolve(__dirname,'dist')//打包出来的文件目录地址,需要是绝对路径,__dirname是项目所在的目录
     // },
     entry: {
-        index:'./src/index.js',
-        other:'./src/other.js'
+        index: './src/index.js',
+        other: './src/other.js'
     },
-    output:{
+    output: {
         filename: '[name].js',
-        path: path.resolve(__dirname,'dist')
+        path: path.resolve(__dirname, 'dist')
     },
 
     devServer: {
         contentBase: path.join(__dirname, 'static'),
         compress: true,//一切服务都启用 gzip 压缩：
         port: 7777,
-        open:true//告诉 dev-server 在 server 启动后打开浏览器。默认禁用。
+        open: true//告诉 dev-server 在 server 启动后打开浏览器。默认禁用。
+    },
+    //=>配置模块加载器LOADER
+    module: {
+        rules: [
+            // //从下往上
+            // {
+            //     test:/\.css$/,
+            //     use:'css-loader'
+            // },
+            // {
+            //     test:/\.css$/,
+            //     use:'style-loader',
+            //     enforce: "post",//pre  优先加载, post  最后加载
+            // }
+            {//简写
+                test:/\.css$/,
+                use:['style-loader','css-loader'],
+            },
+            {//简写
+                test:/\.less$/,
+                use:['style-loader','css-loader','less-loader'],
+            }
+        ]
     },
     plugins: [
         //清空输出目录
