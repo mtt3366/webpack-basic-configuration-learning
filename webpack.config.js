d50d0944a8bb80500a51ed4d397c8423c4f1c2bb
mataotao <mttwind@foxmail.com>
2020-03-20 16:32:02 +0800

    自己造数据,不需要后端

--- webpack.config.js
+++ webpack.config.js
@@ -41,14 +41,19 @@ module.exports = {
         port: 7777,
         open: true,//告诉 dev-server 在 server 启动后打开浏览器。默认禁用。
         hot:true,
-        proxy:{//启动代理,如果说请求的接口都是以api开头的,那么就启动代理
-            '/api':{
-                target:'http://localhost:8000',
-                secure:false,//若为true,则表示以https开头
-                pathRewrite:{'^/api':''},//重写请求的地址,把api开头德替换为空
-                changeOrigin:true//把请求头当中的host改为服务器的地址,不然host不一样,有的服务器不响应
-            }
-        }
+        before: function(app, server, compiler) {//直接启动一个端口号为777的服务
+            app.get('/api/user', function(req, res) {
+                res.json({ custom: 'response' });
+            });
+        },
+        // proxy:{//启动代理,如果说请求的接口都是以api开头的,那么就启动代理
+        //     '/api':{
+        //         target:'http://localhost:8000',
+        //         secure:false,//若为true,则表示以https开头
+        //         pathRewrite:{'^/api':''},//重写请求的地址,把api开头德替换为空
+        //         changeOrigin:true//把请求头当中的host改为服务器的地址,不然host不一样,有的服务器不响应
+        //     }
+        // }
 
     },
     //=>配置模块加载器LOADER
